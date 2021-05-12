4d0e0f3c8ec7c2ae65899d21ac0ca001f36a2e70
Koolbeean <bowmanng@hotmail.com>
2021-05-12 13:30:42 +0800

    Update addressHelpers.test.ts

--- src/__tests__/utils/addressHelpers.test.ts
+++ src/__tests__/utils/addressHelpers.test.ts
@@ -2,8 +2,8 @@ import { getAddress } from 'utils/addressHelpers'
 
 describe('getAddress', () => {
   const address = {
-    56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+    56: '',
+    97: '0xd8b522DBcA4B4E0ba99be9cE8920048291bdE7F0',
   }
 
   it(`get address for mainnet (chainId 56)`, () => {
