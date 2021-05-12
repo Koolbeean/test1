80401bb3877f0c65719400ed9e0d40207715d68e
Koolbeean <bowmanng@hotmail.com>
2021-05-12 23:22:54 +0800

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -82,7 +82,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {t('Cupid Coins')}
         </Heading>
-        <Text>{t('mock test v1')}</Text>
+        <Text>{t('Just want to say Hi')}</Text>
       </Hero>
       <div>
         <Cards>
